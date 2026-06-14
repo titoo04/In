991a59238d1ccd31f42109b3d5e713468e84e4f3
titoo04 <omartamerasu04@gmail.com>
2026-06-14 06:40:24 +0300

    main

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -7,7 +7,7 @@ export default function SiteLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      <main className="flex-1">{children}</main>
+      <main className="flex-1 bg-white">{children}</main>
     </div>
   );
 }
